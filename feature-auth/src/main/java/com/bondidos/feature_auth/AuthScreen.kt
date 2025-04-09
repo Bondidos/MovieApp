ddbbaf22829b962d147c0f7170d301996ea9276a
Uladzimir <UKhadaronak@ibagroup.eu>
2025-04-09 20:10:44 +0300

    auth screen [wip]

--- feature-auth/src/main/java/com/bondidos/feature_auth/AuthScreen.kt
+++ feature-auth/src/main/java/com/bondidos/feature_auth/AuthScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +16,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bondidos.core_navigation_impl.AppNavigator
 import com.bondidos.core_navigation_impl.AppScreen
+import com.bondidos.core_ui.R
 import com.bondidos.core_ui.theme.colors.AppThemeColor
 import com.bondidos.core_ui.theme.MovieAppTheme
+import com.bondidos.core_ui.theme.composables.MoviesAppbar
 
 @Composable
 fun AuthScreen(
@@ -25,23 +29,37 @@ fun AuthScreen(
         modifier = Modifier.background(
             AppThemeColor.APP_BACKGROUND.color()
         ),
-    ) { contentPadding ->
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()
-        ) {
-
-            Text(text = "AuthScreen")
-            ElevatedButton(
-                onClick = {
-                    viewModel.navigateToMovies()
+        topBar = {
+            MoviesAppbar(
+                titleRes = R.string.title_profile,
+                titleTextStyle = MaterialTheme.typography.titleLarge,
+                afterLeadingTitle = R.string.title_sign_up,
+                onAfterLeadingClick = {
+                    print("clicked")
+                    //Todo emit event to model
                 },
+            )
+        },
+        content = { padding ->
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
             ) {
-                Text("Navigate to MoviesFeature")
+
+                Text(text = "AuthScreen")
+                ElevatedButton(
+                    onClick = {
+                        viewModel.navigateToMovies()
+                    },
+                ) {
+                    Text("Navigate to MoviesFeature")
+                }
             }
         }
-    }
+    )
 }
 
 @Preview(backgroundColor = 0xFFD0BCFF)
