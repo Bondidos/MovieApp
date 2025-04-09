6ffcbff2d2945f057239b20b9507dea42cd83dc7
Uladzimir <UKhadaronak@ibagroup.eu>
2025-04-09 13:25:31 +0300

    app module as mediator

--- app/src/main/java/com/bondidos/movieapp/MainActivity.kt
+++ app/src/main/java/com/bondidos/movieapp/MainActivity.kt
@@ -4,30 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.bondidos.core_navigation_impl.AppNavigationGraph
+import com.bondidos.core_navigation_impl.di.NavControllerHolder
 import com.bondidos.core_ui.theme.MovieAppTheme
-import com.bondidos.feature_movies.presentation.testScreen.TestScreen
+import com.bondidos.feature_auth.AuthScreen
+import com.bondidos.feature_movies.presentation.movies_screen.MoviesScreen
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var navControllerHolder: NavControllerHolder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MovieAppTheme {
-                TestScreen()
+                AppNavigationGraph(
+                    authFeature = { AuthScreen() },
+                    moviesFeature = { MoviesScreen() },
+                    navControllerHolder = navControllerHolder,
+                )
             }
         }
     }
 }
-
-@Composable
-@Preview
-fun AppPReview() {
-    MovieAppTheme {
-        TestScreen()
-    }
-}
-
