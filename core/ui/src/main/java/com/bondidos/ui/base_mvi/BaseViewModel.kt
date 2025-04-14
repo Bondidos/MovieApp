5cd54cd11bcdff6160366b62a529f428f9fc753b
Uladzimir <UKhadaronak@ibagroup.eu>
2025-04-14 11:32:21 +0300

    mvi refactored

--- core/ui/src/main/java/com/bondidos/ui/base_mvi/BaseViewModel.kt
+++ core/ui/src/main/java/com/bondidos/ui/base_mvi/BaseViewModel.kt
@@ -1,14 +1,14 @@
 package com.bondidos.ui.base_mvi
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.channels.Channel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 abstract class BaseViewModel<State : Reducer.ViewState, Event : Reducer.ViewEvent, Effect : Reducer.ViewEffect>(
     initialState: State,
@@ -18,30 +18,24 @@ abstract class BaseViewModel<State : Reducer.ViewState, Event : Reducer.ViewEven
     val state: StateFlow<State>
         get() = _state.asStateFlow()
 
-    private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
-    val event: SharedFlow<Event>
-        get() = _event.asSharedFlow()
+    val currentState: State
+        get() = _state.value
 
-    private val _effects = Channel<Effect>(capacity = Channel.CONFLATED)
-    val effect = _effects.receiveAsFlow()
+    private val _effect: MutableSharedFlow<Effect> = MutableSharedFlow()
+    val effect: SharedFlow<Effect>
+        get() = _effect.asSharedFlow()
 
-    fun sendEffect(effect: Effect) {
-        _effects.trySend(effect)
-    }
-
-    fun sendEvent(event: Event) {
-        val (newState, _) = reducer.reduce(_state.value, event)
-
-        _state.tryEmit(newState)
-    }
-
-    fun sendEventForEffect(event: Event) {
+    protected fun reduce(event: Event) {
         val (newState, effect) = reducer.reduce(_state.value, event)
 
         _state.tryEmit(newState)
 
         effect?.let {
-            sendEffect(it)
+            viewModelScope.launch {
+                _effect.emit(it)
+            }
         }
     }
+
+    abstract fun emitIntent(intent: Intention)
 }
--- core/ui/src/main/java/com/bondidos/ui/composables/AppInputTextField.kt
+++ core/ui/src/main/java/com/bondidos/ui/composables/AppInputTextField.kt
@@ -17,9 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bondidos.core_ui.theme.colors.AppThemeColor
 import com.bondidos.ui.R
@@ -75,16 +73,4 @@ fun AppInputTextField(
                 ),
         )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AppInputTextFieldPreview() {
-    AppInputTextField(
-        value = "String",
-        onValueChange = {},
-        labelTextRes = R.string.title_email,
-        leadingIconResId = R.drawable.user_icon,
-        keyBoardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
-    )
 }
