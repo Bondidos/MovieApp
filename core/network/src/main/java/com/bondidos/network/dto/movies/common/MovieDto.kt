017876c0c541ab27101f7486b6546dcac5cfd585
Uladzimir <UKhadaronak@ibagroup.eu>
2025-04-22 13:47:04 +0300

    fix lib versions + query parameters

--- core/network/src/main/java/com/bondidos/network/dto/movies/common/MovieDto.kt
+++ core/network/src/main/java/com/bondidos/network/dto/movies/common/MovieDto.kt
@@ -2,10 +2,29 @@ package com.bondidos.network.dto.movies.common
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import java.util.Date
 
 @JsonClass(generateAdapter = true)
 data class MovieDto(
     @Json(name = "title") val title: String,
     @Json(name = "year") val year: Int,
-    @Json(name = "ids") val ids: MovieIdsDto
+    @Json(name = "ids") val ids: MovieIdsDto,
+    @Json(name = "tagline") val tagline: String,
+    @Json(name = "overview") val overview: String,
+    @Json(name = "released") val released: String,
+    @Json(name = "runtime") val runtime: Int,
+    @Json(name = "country") val country: String,
+    @Json(name = "trailer") val trailer: String?,
+    @Json(name = "homepage") val homepage: String?,
+    @Json(name = "status") val status: String,
+    @Json(name = "rating") val rating: Double,
+    @Json(name = "votes") val votes: Int,
+    @Json(name = "comment_count") val commentCount: Int,
+    @Json(name = "updated_at") val updatedAt: Date,
+    @Json(name = "language") val language: String,
+    @Json(name = "languages") val languages: List<String>,
+    @Json(name = "available_translations") val availableTranslations: List<String>,
+    @Json(name = "genres") val genres: List<String>,
+    @Json(name = "certification") val certification: String?,
+    @Json(name = "original_title") val originalTitle: String
 )
--- core/network/src/main/java/com/bondidos/network/services/TraktApiService.kt
+++ core/network/src/main/java/com/bondidos/network/services/TraktApiService.kt
@@ -3,12 +3,21 @@ package com.bondidos.network.services
 import com.bondidos.network.dto.movies.anticipated.AnticipatedMovieDto
 import com.bondidos.network.dto.movies.trending.TrendingMovieDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TraktApiService {
 
     @GET("movies/trending")
-    suspend fun getTrendingMovies(): List<TrendingMovieDto>
+    suspend fun getTrendingMovies(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int = 10,
+        @Query("extended") extended: String = "full"
+    ): List<TrendingMovieDto>
 
     @GET("movies/anticipated")
-    suspend fun getAnticipatedMovies(): List<AnticipatedMovieDto>
+    suspend fun getAnticipatedMovies(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int = 10,
+        @Query("extended") extended: String = "full"
+    ): List<AnticipatedMovieDto>
 }
