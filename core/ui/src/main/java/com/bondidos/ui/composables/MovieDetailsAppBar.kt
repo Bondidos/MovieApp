ac7313096016c8295a06f584f24f940dd2b5cdfa
Uladzimir <UKhadaronak@ibagroup.eu>
2025-04-26 15:35:27 +0300

    details appbar UI + navigation pop()

--- core/ui/src/main/java/com/bondidos/ui/composables/MovieDetailsAppBar.kt
+++ core/ui/src/main/java/com/bondidos/ui/composables/MovieDetailsAppBar.kt
@@ -1,21 +1,15 @@
 package com.bondidos.ui.composables
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.graphics.Color
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.dp
 import com.bondidos.ui.R
 import com.bondidos.ui.theme.MovieAppTheme
 
@@ -29,7 +23,6 @@ fun MovieDetailsAppBar(
     Row(
         modifier = modifier
             .fillMaxWidth(),
-//            .padding(top = 45.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Image(
