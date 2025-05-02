0f14e21c2a4cfe1e234178ea0c76b96a17f6586d
Uladzimir <UKhadaronak@ibagroup.eu>
2025-05-02 17:44:41 +0300

    tmdb api fix + image model

--- core/network/src/main/java/com/bondidos/network/dto/people/ProfilesDto.kt
+++ core/network/src/main/java/com/bondidos/network/dto/people/ProfilesDto.kt
@@ -1,14 +1,15 @@
 package com.bondidos.network.dto.people
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class ProfilesDto(
-    val aspectRatio: Double?,
-    val height: Int?,
-    val iso6391: String?,
-    val filePath: String?,
-    val voteAverage: Double?,
-    val voteCount: Int?,
-    val width: Int?,
+    @Json(name = "aspect_ratio") val aspectRatio: Double?,
+    @Json(name = "height") val height: Int?,
+    @Json(name = "iso_639_1") val iso6391: String?,
+    @Json(name = "file_path") val filePath: String?,
+    @Json(name = "vote_average") val voteAverage: Double?,
+    @Json(name = "vote_count") val voteCount: Int?,
+    @Json(name = "width") val width: Int?,
 )
--- core/network/src/main/java/com/bondidos/network/interceptors/TmdbApiInterceptor.kt
+++ core/network/src/main/java/com/bondidos/network/interceptors/TmdbApiInterceptor.kt
@@ -8,7 +8,7 @@ class TmdbApiInterceptor: Interceptor {
         val originalRequest = chain.request()
 //todo: Hide api key
         val newRequest = originalRequest.newBuilder()
-            .header("api_key","eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTAwYzQwOWM3ZTg0NzNhYzM0YjcyOTk4MjViNDk1MiIsIm5iZiI6MTc0NDEwODQ4Ny4wMiwic3ViIjoiNjdmNGZiYzcxYmIwNDcyODk5OTk5ZTQyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.9jwnPRAQEMlGfp0nyqBRD8mxM5AKX_hmiogEYDuHYL8")
+            .header("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTAwYzQwOWM3ZTg0NzNhYzM0YjcyOTk4MjViNDk1MiIsIm5iZiI6MTc0NDEwODQ4Ny4wMiwic3ViIjoiNjdmNGZiYzcxYmIwNDcyODk5OTk5ZTQyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.9jwnPRAQEMlGfp0nyqBRD8mxM5AKX_hmiogEYDuHYL8")
             .build()
 
         return chain.proceed(newRequest)
